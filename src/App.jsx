7cea37b157c3b4eee02cc99fe40489bb46e56221
Musafir <askibrahim06@gmail.com>
2026-05-07 19:43:09 +0530

    Initialize Vite React application

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import SemesterPage from './pages/SemesterPage'
+import SubjectPage from './pages/SubjectPage'
+import UploadPage from './pages/UploadPage'
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sem/:semId" element={<SemesterPage />} />
+        <Route path="/sem/:semId/:subjectId" element={<SubjectPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+      </Routes>
+    </HashRouter>
+  )
+}
