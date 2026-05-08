1f6d48cc88bfb08acc8ddfba01c55d975374b11d
Musafir <askibrahim06@gmail.com>
2026-05-08 12:56:56 +0530

    add Vercel Speed Insights and Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 import './App.css'
 import Home from './pages/Home'
 import SemesterPage from './pages/SemesterPage'
@@ -14,6 +16,8 @@ export default function App() {
         <Route path="/sem/:semId/:subjectId" element={<SubjectPage />} />
         <Route path="/upload" element={<UploadPage />} />
       </Routes>
+      <SpeedInsights />
+      <Analytics />
     </HashRouter>
   )
 }
